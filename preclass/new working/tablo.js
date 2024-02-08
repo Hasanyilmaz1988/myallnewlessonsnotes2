9435669fc315f0da488411ab991483db03fffb56
hasanyilmaz1988 <hasanyilmaz1988@icloud.com>
2024-02-08 22:22:44 +0100

    asdds

--- preclass/new working/tablo.js
+++ preclass/new working/tablo.js
@@ -1,9 +1,31 @@
 
 
+var button_1 = document.getElementById("button_1");
+var button_2 = document.getElementById("button_2");
+var button_3 = document.getElementById("button_3");
+var button_4 = document.getElementById("button_4");
+var button_5 = document.getElementById("button_5");
 
 
-var button_ok = document.getElementById("button_ok");
+button_1.onclick = function (){
+    // alert("Görevi bitirmek istediginizden emin misiniz");
+    button_1.style.backgroundColor = "green" ;
+    
+}
 
+button_2.onclick = function (){
+    button_2.style.backgroundColor = "green" ;
+}
 
-button_ok.onclick =function() {
-    caption.style.backgroundColor = "green"
+button_3.onclick = function (){
+    button_3.style.backgroundColor = "green" ;
+}
+
+button_4.onclick = function (){
+    button_4.style.backgroundColor = "green" ;
+}
+
+button_5.onclick = function (){
+    button_5.style.backgroundColor = "green" ;
+}
+
