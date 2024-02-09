0ba6e60bb56ff574bfe32445f676c21b000bd97f
hasanyilmaz1988 <hasanyilmaz1988@icloud.com>
2024-02-09 22:39:42 +0100

    dcs

--- preclass/new working/tablo.js
+++ preclass/new working/tablo.js
@@ -6,26 +6,30 @@ var button_3 = document.getElementById("button_3");
 var button_4 = document.getElementById("button_4");
 var button_5 = document.getElementById("button_5");
 
+var buttons = document.getElementsByClassName("buttons")
 
-button_1.onclick = function (){
-    // alert("Görevi bitirmek istediginizden emin misiniz");
-    button_1.style.backgroundColor = "green" ;
+// button_1.onclick = function (){
+//     // alert("Görevi bitirmek istediginizden emin misiniz");
+//     button_1.style.backgroundColor = "green" ;
     
+// }
+
+// button_2.onclick = function (){
+//     button_2.style.backgroundColor = "green" ;
+// }
+
+// button_3.onclick = function (){
+//     button_3.style.backgroundColor = "green" ;
+// }
+
+// button_4.onclick = function (){
+//     button_4.style.backgroundColor = "green" ;
+// }
+
+// button_5.onclick = function (){
+//     button_5.style.backgroundColor = "green" ;
 }
 
-button_2.onclick = function (){
-    button_2.style.backgroundColor = "green" ;
+buttons[0].onclick = function(){
+    buttons[0].style.backgroundColor = "red";
 }
-
-button_3.onclick = function (){
-    button_3.style.backgroundColor = "green" ;
-}
-
-button_4.onclick = function (){
-    button_4.style.backgroundColor = "green" ;
-}
-
-button_5.onclick = function (){
-    button_5.style.backgroundColor = "green" ;
-}
-
