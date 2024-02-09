ccac116912da04fa7fef4171a2030cb7a2bc8b4b
hasanyilmaz1988 <hasanyilmaz1988@icloud.com>
2024-02-10 00:26:21 +0100

    sadas

--- preclass/new working/tablo.js
+++ preclass/new working/tablo.js
@@ -8,28 +8,42 @@ var button_5 = document.getElementById("button_5");
 
 var buttons = document.getElementsByClassName("buttons")
 
-// button_1.onclick = function (){
-//     // alert("Görevi bitirmek istediginizden emin misiniz");
-//     button_1.style.backgroundColor = "green" ;
+button_1.onclick = function (){
+    confirm("Are you sure?")
+    if (("Are you sure?")) {
+        button_1.style.backgroundColor = "green"}
+    else{button_1.style.backgroundColor = "red"}
+    }
     
-// }
+   
+    
 
-// button_2.onclick = function (){
-//     button_2.style.backgroundColor = "green" ;
-// }
 
-// button_3.onclick = function (){
-//     button_3.style.backgroundColor = "green" ;
-// }
-
-// button_4.onclick = function (){
-//     button_4.style.backgroundColor = "green" ;
-// }
-
-// button_5.onclick = function (){
-//     button_5.style.backgroundColor = "green" ;
+button_2.onclick = function (){
+    button_2.style.backgroundColor = "green" ;
 }
 
-buttons[0].onclick = function(){
-    buttons[0].style.backgroundColor = "red";
+button_3.onclick = function (){
+    button_3.style.backgroundColor = "green" ;
 }
+
+button_4.onclick = function (){
+    button_4.style.backgroundColor = "green" ;
+}
+
+button_5.onclick = function (){
+    button_5.style.backgroundColor = "green" ;
+}
+
+// buttons[0].onclick = function(){
+//     confirm("Emin misiniz?");
+//     buttons[0].style.backgroundColor = "red";
+// }
+
+// buttons[5].onclick = function(){
+//     if (confirm("Emin misiniz?")) {
+//         buttons[5].style.backgroundColor = "blue";
+//     } else {
+//         buttons[5].style.backgroundColor = "red";
+//         } 
+//     }
