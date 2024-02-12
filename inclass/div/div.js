0d5bd10aa7f1cef0e7ccacde0bf2480b792fdf0f
hasanyilmaz1988 <hasanyilmaz1988@icloud.com>
2024-02-12 22:45:56 +0100

    sda

---
+++ inclass/div/div.js
@@ -0,0 +1,27 @@
+var div_1 = document.getElementById("div_1")
+var div_2 = document.getElementById("div_2")
+var main_container = document.getElementById("main_container")
+
+div_1.onclick = function() {
+    div_2.style.backgroundColor = "red";
+}
+
+// div_2.onmouseenter = function(){
+//     div_1.style.backgroundColor = "black";
+// }
+
+// div_2.onmouseout = function(){
+//     div_1.style.backgroundColor = "white";
+// }
+
+div_1.onmouseenter = function(){
+     div_2.style.backgroundColor = "yellow";
+    if (div_2.style.backgroundColor == "yellow") {
+        main_container.style.backgroundColor = "orange";
+    }
+}
+
+div_1.onmouseout = function(){
+    div_2.style.backgroundColor = "brown";
+    main_container.style.backgroundColor = "blue"
+}
